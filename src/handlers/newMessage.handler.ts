4369b768fee13905c4589399f022e9c8d25aa731
Shashank B <shashank1050269@gmail.com>
2023-09-01 22:09:07 +0530

    Handle bidding process

--- src/handlers/newMessage.handler.ts
+++ src/handlers/newMessage.handler.ts
@@ -1,21 +1,70 @@
-export const newMessageHandler = (client) => {
-  client.on("message", async (message) => {
-    //check whether the current chat is the chit group
-    const chat = await message.getChat();
-    const groupNameRegex: RegExp =
-      /^(?:[1-9]\d{0,3}|[1-9]\d{0,4}|[1-9]\d{0,5})M\*(?:[1-9]\d{0,4}|[1-9]\d{0,5})K=(?:[1-9]\d{0,5}|[1-9]\d{0,6})L-StDt-(?:[1-9]|[12]\d|3[01])-([1-9]|1[0-2])-(202[3-9]|2030)$/;
+import { Chat, Client, Contact, Message } from "whatsapp-web.js";
 
-    if (groupNameRegex.test(chat)) {
-      console.log("Received message in the group", chat.name);
-      //match the message for number,number+'k'
-      const bidAmountRegex = /^(\d+(\.\d+)?|(\d+(\.\d+)?)[kK])$/;
-      const bidAmountValue = bidAmountRegex.test(message.body)
-        ? parseFloat(message.body)
-        : null;
+let currentHighestBid: number = 0;
+let bidding: boolean = true;
 
-      if (bidAmountValue) {
-        console.log("Current bid is", bidAmountValue);
-      }
+export const newMessageHandler = (client: Client) => {
+  client.on("message", async (message: Message) => {
+    const chat: Chat = await message.getChat();
+    if (bidding) {
+      await startBiddingProcess(chat, message);
     }
   });
+};
+
+const startBiddingProcess = async (chat: Chat, message: Message) => {
+  const groupNameRegex: RegExp =
+    /^(?:[1-9]\d{0,3}|[1-9]\d{0,4}|[1-9]\d{0,5})M\*(?:[1-9]\d{0,4}|[1-9]\d{0,5})K=(?:[1-9]\d{0,5}|[1-9]\d{0,6})L-StDt-(?:[1-9]|[12]\d|3[01])-([1-9]|1[0-2])-(202[3-9]|2030)$/;
+
+  if (groupNameRegex.test(chat.name)) {
+    console.log("Received message in the group", chat.name);
+
+    const bidAmountRegex: RegExp = /^(\d+(\.\d+)?|(\d+(\.\d+)?)[kK])$/;
+    const bidAmountValue: number = bidAmountRegex.test(message.body)
+      ? parseFloat(message.body)
+      : null;
+
+    const bidAmount: number =
+      bidAmountValue.toString().length >= 5 &&
+      bidAmountValue.toString().indexOf(".") == -1
+        ? bidAmountValue
+        : bidAmountValue * 1000;
+
+    if (bidAmount) {
+      console.log("Current bid is", bidAmountValue);
+      if (bidAmount < currentHighestBid) {
+        message.reply(
+          "Please ensure you bid" +
+            bidAmount +
+            " is greater than the highest bid amount of " +
+            currentHighestBid
+        );
+        return;
+      }
+      currentHighestBid =
+        bidAmount > currentHighestBid ? bidAmount : currentHighestBid;
+      setTimeout(
+        async () => await chat.sendMessage(currentHighestBid + " once"),
+        120000
+      );
+      setTimeout(
+        async () => await chat.sendMessage(currentHighestBid + " twice"),
+        120000
+      );
+      await chat.sendMessage("Any more bid from anyone?");
+      await chat.sendMessage("Will wait for 2 mins and then close");
+
+      setTimeout(async () => {
+        await chat.sendMessage(currentHighestBid + " thrice");
+        bidding = false;
+      }, 120000);
+
+      const contact: Contact = await message.getContact();
+
+      chat.sendMessage(`${contact.id.user}, this month's chit is yours.`, {
+        mentions: [contact],
+      });
+    }
+  }
+  bidding = false;
 };
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import { newMessageHandler } from "./handlers/newMessage.handler";
 
 const client: Client = new Client({});
 
+// Generate the QR to enable user login
 client.on("qr", (qr) => {
   qrCode.generate(qr, { small: true });
 });
 
+// Handle the operations once the session is active
 client.on("ready", () => {
   console.log("Client is ready");
 });
